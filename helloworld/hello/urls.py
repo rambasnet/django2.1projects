c2776f52ecb41b021e2103789b309ff6497566d8
Ram Basnet <rambasnet@gmail.com>
2018-10-29 18:56:37 -0600

    modified hello

--- helloworld/hello/urls.py
+++ helloworld/hello/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^index/$', views.index, name='index')
+    re_path(r'^index/$', views.index, name='index'),
+    re_path(r'^bye/$', views.bye, name='bye'),
+    re_path(r'^time/$', views.current_datetime, name='time')
 ]
--- helloworld/hello/views.py
+++ helloworld/hello/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hello there wonderful world...')
+
+def bye(request):
+    return HttpResponse('Adios...until next time...')
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '''<!DOCTYPE html>
+            <html>
+            <head>
+                <title>Current Server Datetime</title>
+                <style>
+                    body {{
+                        background-color: lightblue;
+                    }}
+                    p {{
+                        font-size: 1.5em;
+                        color: green;
+                    }}
+                </style>
+            </head>
+            <body>
+                <h1>{}</h1>
+                <p>It is now {}.</p>
+            </body>
+            </html>
+            '''.format('Date Time', now)
+    return HttpResponse(html)
 
     
